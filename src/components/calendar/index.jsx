5afa873fe392ab690dbf4c2b2e756c585e8e14a3
cargando <cargando@mail.ru>
2018-12-18 21:47:57 +0300

    added calendar template

---
+++ src/components/calendar/index.jsx
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export class Calendar extends Component {
+	static propTypes = {
+		handleAdd: PropTypes.func, // метод добавления напоминания  в список
+	};
+
+	constructor(props, context) {
+		super(props, context);
+	}
+
+
+
+		render() {
+			return (
+				<div id='calendar' className='micalendar' style='display:none'>
+					<div className='header_wrap'>
+						<div className='header'>
+							<p id='monthHeader'>Month name</p>
+						</div>
+						<div className='arrows'>
+							<div id='prevButton' className='arrows_left'><i className='fa fa-angle-left'></i></div>
+							<div id='nextButton' className='arrows_right'><i className='fa fa-angle-right'></i></div>
+						</div>
+					</div>
+					<table id='calendar_table'>
+						<thead>
+						<tr>
+							<th>Пн</th>
+							<th>Вт</th>
+							<th>Ср</th>
+							<th>Чт</th>
+							<th>Пт</th>
+							<th>Сб</th>
+							<th>Вс</th>
+						</tr>
+						</thead>
+						<tbody>
+						</tbody>
+					</table>
+					<hr />
+						<div className='btn-group'>
+							<input type='button' onClick='hideCalendar(event)' value='Отмена'
+							       className='btn btn-sm btn-outline-secondary' />
+						</div>
+				</div>);
+		}
+	}
